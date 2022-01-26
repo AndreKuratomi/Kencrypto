7ed0a72bb0802d253eee5e8458d2362c7a8ec873
André Kuratomi <andrekuratomi@gmail.com>
2022-01-26 20:03:08 -0300

    feat: some failed Error attempts drafted

--- src/api/coinMarketCap.ts
+++ src/api/coinMarketCap.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
 import Quotation from "../types/quotes.types";
 import Conversion from "../types/conversions.types";
+import Error from "../types/errors.types";
 
 export default class CoinMarketCap {
   baseURL: string = "https://pro-api.coinmarketcap.com";
@@ -17,37 +18,52 @@ export default class CoinMarketCap {
 
   async quotes(symbols: string[]) {
     const requestURL = `/v1/cryptocurrency/quotes/latest?symbol=${symbols}`;
-    const response: AxiosResponse<any, any> = await this.axiosInstance.get(
-      requestURL
-    );
 
-    // "Your parsing code should explicitly parse only the response properties you require to guarantee new fields that may be returned in the future are ignored."
+    try {
+      const response: AxiosResponse<any, any> = await this.axiosInstance.get(
+        requestURL
+      );
 
-    // const quotesResponseModel = {
-    //   data: {
-    //     id: 1,
-    //     symbol: "BTC",
-    //     name: "Bitcoin",
-    //     amount: 25.67,
-    //     last_updated: "2021-08-26T18:30:17.000Z",
-    //     quote: {
-    //       ETH: {
-    //         price,
-    //         last_updated,
-    //       },
-    //     },
-    //   },
-    // };
+      // "Your parsing code should explicitly parse only the response properties you require to guarantee new fields that may be returned in the future are ignored."
 
-    return response.data as Quotation;
+      // const quotesResponseModel = {
+      //   data: {
+      //     id: 1,
+      //     symbol: "BTC",
+      //     name: "Bitcoin",
+      //     amount: 25.67,
+      //     last_updated: "2021-08-26T18:30:17.000Z",
+      //     quote: {
+      //       ETH: {
+      //         price,
+      //         last_updated,
+      //       },
+      //     },
+      //   },
+      // };
+
+      return response.data as Quotation;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        return err.response?.data as Error;
+      }
+      return undefined;
+    }
   }
 
   async conversions(amount: number, symbol: string, convert: string[]) {
     const requestURL = `/v1/tools/price-conversion?amount=${amount}&symbol=${symbol}&convert=${convert}`;
-    const response: AxiosResponse<any, any> = await this.axiosInstance.get(
-      requestURL
-    );
+    try {
+      const response: AxiosResponse<any, any> = await this.axiosInstance.get(
+        requestURL
+      );
 
-    return response.data as Conversion;
+      return response.data as Conversion;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        return err.response?.data as Error;
+      }
+      return undefined;
+    }
   }
 }
