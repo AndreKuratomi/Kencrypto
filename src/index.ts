881ce5fc8079d862b0dc3de29de6027e49b66722
André Kuratomi <andrekuratomi@gmail.com>
2022-01-26 21:00:29 -0300

    feat: aplication updated to prepare to be exported

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
 import CoinMarketCap from "./api/coinMarketCap";
+import Conversion from "./types/conversions.types";
+import Quotes from "./types/quotes.types";
+import Error from "./types/errors.types";
 import { isError404 } from "./types/errors.types";
 
-const newCoin = new CoinMarketCap("a421d3d1-9c6b-444b-9e79-bd80e558aea8");
+export { CoinMarketCap };
+export { Conversion, Quotes, Error, isError404 };
 
-newCoin.quotes(["BTC", "NGN"]).then((resp) => {
-  if (resp !== undefined) {
-    if (isError404(resp)) {
-      resp.status;
-    } else {
-      console.log(resp.data);
-    }
-  }
-  // console.log(resp.data);
-  // tentar passar para arquivo JSON
-});
+// const newCoin = new CoinMarketCap("a421d3d1-9c6b-444b-9e79-bd80e558aea8");
 
-newCoin.conversions(1000, "NGN", ["NZD"]).then((resp) => {
-  if (resp !== undefined) {
-    if (isError404(resp)) {
-      resp.status;
-    } else {
-      console.log(resp.data);
-    }
-  }
-  // console.log(resp.data);
-});
+// newCoin.quotes(["BTC", "NGN"]).then((resp) => {
+//   if (resp !== undefined) {
+//     if (isError404(resp)) {
+//       resp.status;
+//     } else {
+//       console.log(resp.data);
+//     }
+//   }
+//   // console.log(resp.data);
+//   // tentar passar para arquivo JSON
+// });
+
+// newCoin.conversions(1000, "NGN", ["NZD"]).then((resp) => {
+//   if (resp !== undefined) {
+//     if (isError404(resp)) {
+//       resp.status;
+//     } else {
+//       console.log(resp.data);
+//     }
+//   }
+//   // console.log(resp.data);
+// });
