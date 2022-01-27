5d2f07234da5a2fcbc308febc9dd7a782594ba2d
André Kuratomi <andrekuratomi@gmail.com>
2022-01-27 17:04:58 -0300

    fix: types' syntax corrected

--- src/api/coinMarketCap.ts
+++ src/api/coinMarketCap.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
-import Quotation from "../types/quotes.types";
+import Quotes from "../types/quotes.types";
 import Conversion from "../types/conversions.types";
 import Error from "../types/errors.types";
 
@@ -22,28 +22,28 @@ export default class CoinMarketCap {
     const response: AxiosResponse<any, any> = await this.axiosInstance.get(
       requestURL
     );
-    // console.log(response.data);
 
-    // const currency = symbols;
+    const currency: string[] = symbols;
 
-    // const obj = {};
-    // obj["data"] = {};
+    const obj: any = {};
+    obj["data"] = {};
 
-    // for (let i = 0; i < currency.length; i++) {
-    //   obj.data[currency[i]] = {};
-    //   obj.data[[currency[i]]]["id"] = response.data[currency[i]]["id"];
-    //   obj.data[[currency[i]]]["name"] = response.data[currency[i]]["name"];
-    //   obj.data[[currency[i]]]["symbol"] = response.data[currency[i]]["symbol"];
-    //   obj.data[[currency[i]]]["slug"] = response.data[currency[i]]["slug"];
-    //   obj.data[[currency[i]]]["date_added"] =
-    //     response.data[currency[i]]["date_added"];
-    //   obj.data[[currency[i]]]["last_updated"] =
-    //     response.data[currency[i]]["last_updated"];
-    //   obj.data[[currency[i]]]["quote"] = response.data[currency[i]]["quote"];
-    // }
+    for (let i = 0; i < currency.length; i++) {
+      obj.data[currency[i]] = {};
+      obj.data[currency[i]]["id"] = response.data.data[currency[i]]["id"];
+      obj.data[currency[i]]["name"] = response.data.data[currency[i]]["name"];
+      obj.data[currency[i]]["symbol"] =
+        response.data.data[currency[i]]["symbol"];
+      obj.data[currency[i]]["slug"] = response.data.data[currency[i]]["slug"];
+      obj.data[currency[i]]["date_added"] =
+        response.data.data[currency[i]]["date_added"];
+      obj.data[currency[i]]["last_updated"] =
+        response.data.data[currency[i]]["last_updated"];
+      obj.data[currency[i]]["quote"] = response.data.data[currency[i]]["quote"];
+    }
 
-    // return obj as Quotation;
-    return response.data as Quotation;
+    return obj as Quotes;
+    // return response.data as Quotation;
     // try {
     // } catch (err) {
     //   if (axios.isAxiosError(err)) {
@@ -58,38 +58,6 @@ export default class CoinMarketCap {
     const response: AxiosResponse<any, any> = await this.axiosInstance.get(
       requestURL
     );
-
-    // const elements = [amount, convert, symbol];
-
-    // const obj = {};
-    // obj["data"] = {};
-
-    // for (let elem in response.data) {
-    //   obj.data[elements[i]] = {};
-    //   obj.data[[elements[i]]]["id"] = response.data[elements[i]]["id"];
-    //   obj.data[[elements[i]]]["name"] = response.data[elements[i]]["name"];
-    //   obj.data[[elements[i]]]["symbol"] = response.data[elements[i]]["symbol"];
-    //   obj.data[[elements[i]]]["amount"] = response.data[elements[i]]["amount"];
-    //   obj.data[[elements[i]]]["last_updated"] =
-    //     response.data[elements[i]]["last_updated"];
-    //   obj.data[[elements[i]]]["quote"] = response.data[elements[i]]["quote"];
-    // }
-
-    // const newObject = {
-    //   data: {
-    //     id: 1,
-    //     symbol: "BTC",
-    //     name: "Bitcoin",
-    //     amount: 25.67,
-    //     last_updated: "2021-08-26T18:30:17.000Z",
-    //     quote: {
-    //       ETH: {
-    //         price: 386.5352847529818,
-    //         last_updated: "2021-08-26T18:30:16.000Z",
-    //       },
-    //     },
-    //   },
-    // };
 
     return response.data as Conversion;
     // try {
